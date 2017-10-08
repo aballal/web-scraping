bb3c0e354782ee6d551e0e6a7205ab8be74f664c
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-10-08 08:30:01 +0100

    Refactored for readability

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,13 +2,13 @@
 class Scraper
   def initialize(page)
     @page = page
+    @listing_page = page
   end
 
   def job_details
     job_links.map do |link|
-      @page = link.click
-      indices = search_texts.map { |search_text| texts.find_index { |text| text == search_text } }
-      [texts[0]] + indices.collect { |i| texts[i + 1] }
+      @details_page = link.click
+      selected_texts
     end
   end
 
@@ -18,34 +18,37 @@ class Scraper
 
   private
 
-  attr_reader :page
+  attr_reader :page, :listing_page, :details_page
 
-  # Returns an array of links to each job's page
+  # Returns an array of links to each job's individual page
   def job_links
     links = []
     loop do
-      links << page.links_with(class: 'ovalbuttondetails')
+      links << listing_page.links_with(class: 'ovalbuttondetails')
       break unless next_link
-      @page = next_link.click
+      @listing_page = next_link.click
     end
     links.flatten
   end
 
   def next_link
-    page.links_with(text: 'Next >').first
+    listing_page.links_with(text: 'Next >').first
   end
 
-  def search_texts
-    ['Recruiter:', 'Salary:']
-  end
-
-  # Returns all texts on a job's individual page
-  def texts
-    page
+  def all_texts
+    details_page
       .search('#ctl00_ContentPlaceHolder1_DisplayVacancyUC1_pnlRounded')
       .inner_text
       .split("\n")
       .map(&:strip)
       .reject { |text| text == '' }
+  end
+
+  def selected_texts
+    texts = all_texts
+    keys = ['Recruiter:', 'Salary:']
+    key_indices = keys.map { |key| texts.find_index { |text| text == key } }
+    values = key_indices.collect { |i| texts[i + 1] }
+    [texts[0]] + values
   end
 end
